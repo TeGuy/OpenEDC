ccdc29bf5179deede1c333aaf6fec35dfa090b6d
Philipp Neuhaus <philipp.neuhaus@uni-muenster.de>
2021-02-26 09:56:49 +0100

    fixed url

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 //Testquery
 var invocation = new XMLHttpRequest();
 
-invocation.open('GET', repository.getDownloadUrl(parameter), false);
+invocation.open('GET', 'https://mdmj-staging.uni-muenster.de/41594/download?format=odm&form-lang=en', false);
 invocation.withCredentials = true;
 //invocation.onreadystatechange = handler;
 invocation.send();
