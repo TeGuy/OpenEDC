7333bb573dc0823f84341b969667e4a89b4b36f8
Philipp Neuhaus <philipp.neuhaus@uni-muenster.de>
2021-02-26 09:17:40 +0100

    Tried to clarify fetch statement

--- js/helper/repositoryhelper.js
+++ js/helper/repositoryhelper.js
@@ -21,6 +21,16 @@ export const getParameterNames = () => {
 export const getModel = async (parameterName, parameter) => {
     const repository = repositories.find(repository => repository.parameterName == parameterName);
 
+    //Testquery
+    var invocation = new XMLHttpRequest();
+    var url = 'http://bar.other/resources/credentialed-content/';
+
+    invocation.open('GET', repository.getDownloadUrl(parameter), true);
+    invocation.withCredentials = true;
+    //invocation.onreadystatechange = handler;
+    invocation.send();
+
+
     const response = await fetch(repository.getDownloadUrl(parameter), {
         method: 'GET',
         mode: 'cors',
